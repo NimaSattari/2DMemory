13e6a5cd756f2557f3baa8dfce45317c50c6a892
Nima <54527075+NimaSattari@users.noreply.github.com>
2021-06-23 17:00:38 +0430

    Initial commit

---
+++ Assets/Scripts/Music.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Music : MonoBehaviour
+{
+    [SerializeField] AudioClip MusicAudio;
+
+    void Awake()
+    {
+        PlaySong();
+        SetUpSingleton();
+    }
+    void PlaySong()
+    {
+        AudioClip audio = MusicAudio;
+        GetComponent<AudioSource>().PlayOneShot(audio);
+        Invoke("PlayNextSong", audio.length);
+    }
+
+    private void SetUpSingleton()
+    {
+        if (FindObjectsOfType(GetType()).Length > 1)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+}
